86b018cec97a2cea2d65a49892befd1d0ad78c3a
imran65545 <cimran1307@gmail.com>
2025-04-14 13:07:18 +0530

    added songs in json

--- script.js
+++ script.js
@@ -30,94 +30,79 @@ function formatTime(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    const response = await fetch(`${folder}/`);
 
+    // Fetch info.json for album and songs
+    const infoResponse = await fetch(`${folder}/info.json`);
+    const infoData = await infoResponse.json();
 
-    let text = await response.text();
-    let div = document.createElement("div");
-    div.innerHTML = text;
-  
-    let as = div.getElementsByTagName("a");
-     songs = [];
-  
-    for (let element of as) {
-      if (element.href.endsWith(".mp3")) {
-        songs.push(decodeURIComponent(element.href.split(`/${folder}/`)[1]));
-      }
-    }
+    const albumTitle = infoData.title || "MyPhonk";
+    const albumDescription = infoData.description || "Phonk Playlist";
+    const songsData = infoData.songs || [];
 
-    //   // Array of cover photos corresponding to each song
-      coverPhotos = [
+    // Set album title and description
+    document.querySelector(".album-title").textContent = albumTitle;
+    document.querySelector(".album-description").textContent = albumDescription;
+
+    // Define covers (Make sure they match song count or default)
+    const coverPhotos = [
         "https://i.scdn.co/image/ab67616d00004851611ae53864c78e196dd5f555",
-            "https://i.scdn.co/image/ab67616d00004851c771f7401307f639d0272f68",
-            "https://i.scdn.co/image/ab67616d000048515443e7ddafe075e963a6bd34",
-            "https://i.scdn.co/image/ab67616d00004851ecb8455c697c79e12445b4a0",
-            "https://i.scdn.co/image/ab67616d00004851144fcad8ffb348edefb9e851",
-            "https://i.scdn.co/image/ab67616d000048517b83f01b12e6c12b328d7a45",
-            "https://i.scdn.co/image/ab67616d000048519bf5280998453fee06ab1c6a",
-            "https://i.scdn.co/image/ab67616d000048517c870eecd4fbd6d8dd618f48", // Cover for the third song
-        // Add more covers for all songs
+        "https://i.scdn.co/image/ab67616d00004851c771f7401307f639d0272f68",
+        "https://i.scdn.co/image/ab67616d000048515443e7ddafe075e963a6bd34",
+        "https://i.scdn.co/image/ab67616d00004851ecb8455c697c79e12445b4a0",
+        "https://i.scdn.co/image/ab67616d00004851144fcad8ffb348edefb9e851",
+        "https://i.scdn.co/image/ab67616d000048517b83f01b12e6c12b328d7a45",
+        "https://i.scdn.co/image/ab67616d000048519bf5280998453fee06ab1c6a",
+        "https://i.scdn.co/image/ab67616d000048517c870eecd4fbd6d8dd618f48",
+        "https://i.scdn.co/image/ab67616d00004851fc8a3e0f0f2016068474bdc9",
+        "https://i.scdn.co/image/ab67616d00004851c80fe27586c2da910984cb9a",
+        "https://i.scdn.co/image/ab67616d00004851ecf02d417f93c338e54f77b4",
+        "https://i.scdn.co/image/ab67616d00004851e4abb0fb75117230282cd98c",
+        "https://i.scdn.co/image/ab67616d00004851361d4c29897bec6ef9a363b6",
+        "https://i.scdn.co/image/ab67616d00004851550c08bd67e6d175e2dc463b",
+        "https://i.scdn.co/image/ab67616d000048516e415c25972cc9c91dd1034c",
+        "https://i.scdn.co/image/ab67616d0000485193942466c2c6ec14557522c6"
     ];
 
-    coverPhotos2 = ["https://i.scdn.co/image/ab67616d00004851fc8a3e0f0f2016068474bdc9",// Cover for the first song 
-                "https://i.scdn.co/image/ab67616d00004851c80fe27586c2da910984cb9a", // Cover for the second song
-                "https://i.scdn.co/image/ab67616d00004851ecf02d417f93c338e54f77b4",
-                "https://i.scdn.co/image/ab67616d00004851e4abb0fb75117230282cd98c",
-                "https://i.scdn.co/image/ab67616d00004851361d4c29897bec6ef9a363b6",
-                "https://i.scdn.co/image/ab67616d00004851550c08bd67e6d175e2dc463b",
-                "https://i.scdn.co/image/ab67616d000048516e415c25972cc9c91dd1034c",
-                "https://i.scdn.co/image/ab67616d0000485193942466c2c6ec14557522c6",
+    // Inject songs into DOM
+    const songList = document.querySelector(".songName ul");
+    songList.innerHTML = "";
 
-    ]
+    for (let i = 0; i < songsData.length; i++) {
+        const song = songsData[i];
+        const cover = coverPhotos[i] || "default-cover.jpg";
 
-    
-
-    console.log("Fetched songs:", songs);
-
-    
-    let songList = document.querySelector(".songName").getElementsByTagName("ul")[0];
-    songList.innerHTML = ""; // Clear any existing list
-
-    for (let i = 0; i < songs.length; i++) {
-        let songName = songs[i].replaceAll("%20", " ");
-        let SongName = songs[i].replaceAll("Download.mp3", "");
-        let coverPhoto = coverPhotos[i] || "default-cover.jpg";
-
-        // Add each song as an HTML list item
         songList.innerHTML += `
             <li>
-                <img src="${coverPhoto}" alt="${songName,SongName}" class="song-cover" />
+                <img src="${cover}" alt="${song.title}" class="song-cover" />
                 <div class="song-info">
-                    <div>${songName}</div>
+                    <div>${song.title}</div>
                     <div class="playnow">
                         <span>Play Now</span>
-                        <i  class="fa-solid fa-play"></i>
+                        <i class="fa-solid fa-play"></i>
                     </div>
                 </div>
             </li>
         `;
     }
 
-    // Attach event listeners to each song
-    Array.from(document.querySelector(".songName").getElementsByTagName("li")).forEach((e, index) => {
-        e.addEventListener("click", () => {
-            console.log(`Playing: ${songs[index]}`);
-            playMusic(songs[index],coverPhotos[index]);
+    // Add click events for each song
+    const items = document.querySelectorAll(".songName li");
+    items.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            console.log(`Playing: ${songsData[index].filename}`);
+            playMusic(songsData[index].filename, coverPhotos[index]);
         });
-
     });
 
-
-    if (songs.length > 0) {
-        currentSongIndex = 0; // Start with the first song
-        playMusic(songs[0], coverPhotos[0]);
-        currentSong.pause() // Play the first song by default
-        PLAY.querySelector("i").className ="fa-solid fa-play";
+    // Autoplay first song (paused)
+    if (songsData.length > 0) {
+        currentSongIndex = 0;
+        playMusic(songsData[0].filename, coverPhotos[0]);
+        currentSong.pause();
+        PLAY.querySelector("i").className = "fa-solid fa-play";
     }
+}
 
-    
-   
-  }
 
   const playMusic = (track,coverPhoto) => {
     
